484cdfb4ae20b3c3c7ae876dfa033d0cc4ac6ed9
Mirza <mirzaikram129@gmail.com>
2024-10-09 05:09:19 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   reactStrictMode: true,
 
-  // Set the output to 'export' for static export
-  output: 'export',
-
-  // Disable image optimization for static export
-  images: {
-    unoptimized: true,
-  },
-
   webpack(config, { isServer }) {
     if (!isServer) {
       config.devServer = {
