a422df72aff717302a0ae9facd4728109eae1baa
Mirza <mirzaikram129@gmail.com>
2024-10-14 11:55:53 +0530

    updated stuff

--- utils/smsService.js
+++ utils/smsService.js
@@ -1,14 +1,14 @@
 import twilio from "twilio";
 
-const accountSid = "AC88752ba003e54d3b091d2813f0382419"; // Twilio Account SID
-const authToken = "a01abf563a8b6cc140346da8d900da10"; // Twilio Auth Token
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Twilio Account SID
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Twilio Auth Token
 const twilioClient = twilio(accountSid, authToken);
 
 export const sendSMS = async (to, message) => {
   try {
     await twilioClient.messages.create({
       body: message,
-      from: +13148889196, // Your Twilio phone number
+      from: twilioPhoneNumber, // Your Twilio phone number
       to,
     });
     console.log("Message sent:", messageResponse.sid);
