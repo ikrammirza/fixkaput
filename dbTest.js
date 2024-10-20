7169ffa6d0ba93e8ad02d0d2060fd03e6708f3ad
Mirza <mirzaikram129@gmail.com>
2024-10-20 11:08:59 +0530

    updated stuff

--- dbTest.js
+++ dbTest.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
-const uri = 'mongodb+srv://mirzaikram129:Jabir%23%2412@fixkaput.ai3gi.mongodb.net/mydatabase?retryWrites=true&w=majority';
+const mongoose = require("mongoose");
+const uri =
+  "mongodb+srv://mirzaikram129:Jabir%23%2412@fixkaput.ai3gi.mongodb.net/mydatabase?retryWrites=true&w=majority";
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Database connection successful');
+    console.log("Database connection successful");
   })
-  .catch(err => {
-    console.error('Database connection error:', err);
+  .catch((err) => {
+    console.error("Database connection error:", err);
   });
