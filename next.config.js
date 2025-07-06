b4a312bbc72ffe356f3ab8db9d55fc96a9098fd1
Mirza <mirzaikram129@gmail.com>
2025-07-06 06:42:28 +0530

    updated bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-  
+  images: {
+    domains: ['images.pexels.com'],
+  },
   // If you need to extend Webpack's configuration, do it here
   webpack(config, { isServer }) {
     // Add custom webpack configurations here only if necessary
